ffad7cb4518936c34870253ff3efcf28dbb9b9a4
Goutham Duduka <gouthamduduka@gmail.com>
2020-05-31 12:23:31 +0530

    Added methods required for Hotel Details page

--- src/main/java/repo/HotelDetailsRepo.java
+++ src/main/java/repo/HotelDetailsRepo.java
@@ -7,4 +7,6 @@ public interface HotelDetailsRepo {
 
     public By roomsTab = By.id("detpg_hotel_rooms");
     public By roomsType = By.id(".roomWrap h2"); //get list and retrieve innerText from first index
+    public By hotelName = By.id("detpg_hotel_name");
+    public By selectRoomButton = By.cssSelector("div.roomRight div.roomTypePrice a");
 }
